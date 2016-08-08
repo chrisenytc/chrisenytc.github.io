34778ac2341faf4477b212f25a9e80e1ba2ffe6c
Christopher EnyTC <chris@enytc.com>
2016-08-08 07:24:30 -0300

    Update assets

--- assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-  
-//------- SITE LOADER ---//
-
-jQuery(window).load(function() { 
-    jQuery(".spinner").delay(400).fadeOut("slow");
-    jQuery(".title-load").delay(400).fadeOut("slow");
-    jQuery("#loader").delay(800).fadeOut("slow"); 
-});
-
